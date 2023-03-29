1ac6d14b9adcdef087d99797aa90361ae0cfb6a3
ayghub <91691648+ayghub@users.noreply.github.com>
2023-03-29 17:58:14 +0000

    added two functions check_file and log

--- main.lua
+++ main.lua
@@ -1,11 +1,28 @@
--- the destination folder. note escaping the file path.
-dst = "D:\\V\\Watched"
+dst = [[D:\V\watched]]
+
+function log(level,string,secs)
+  secs = secs or 2.5
+  mp.msg.log(level,string)
+  mp.osd_message(string,secs)
+end
+
+function check_file() 
+  local filepath = mp.get_property('path')
+  local _, _, proto = filepath:find("^(%a+)://")
+  if proto then
+    log("warn",string.format("Cannot move a streaming file", proto))
+    return nil
+  end
+  return filepath
+end
 
 function move_to()
-  local filepath = mp.get_property('path')
-  local cmd = string.format('move "%s" "%s"', filepath, dst)
-  mp.msg.info("the file is moved successfully, or not")
-  os.execute(cmd)
+  local filepath = check_file()
+  if filepath then
+    local cmd = string.format('move "%s" "%s"', filepath, dst)
+    log("info",string.format("moving %s to %s", filepath,dst))
+    os.execute(cmd)
+  end 
 end
 
 mp.add_key_binding('ctrl+m', "move-file", move_to)
