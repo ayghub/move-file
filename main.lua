53232fe44f0b5778983f45f09483482c0e300b72
mrvf <44103714+yassin-l@users.noreply.github.com>
2023-08-07 16:52:30 +0100

    Fix error by creating the destination folder if it doesn't exist and add Linux support
    - Create the destination folder if it doesn't exist to prevent errors
    - Modify the cmd value based on the platform to support Linux

--- main.lua
+++ main.lua
@@ -1,28 +1,52 @@
-dst = [[D:\V\watched]]
+local utils = require "mp.utils"
+dst = [[/home/mrvf/some]]
 
 function log(level,string,secs)
-  secs = secs or 2.5
-  mp.msg.log(level,string)
-  mp.osd_message(string,secs)
+	secs = secs or 2.5
+	mp.msg.log(level,string)
+	mp.osd_message(string,secs)
 end
 
-function check_file() 
-  local filepath = mp.get_property('path')
-  local _, _, proto = filepath:find("^(%a+)://")
-  if proto then
-    log("warn",string.format("Cannot move a streaming file", proto))
-    return nil
-  end
-  return filepath
+function check_dir() 
+	if not utils.file_info(dst) then
+		os.execute("mkdir "..dst)
+		print("created folder "..dst)
+	end
+end
+
+function check_file(path) 
+	local filepath = mp.get_property('path')
+	local _, _, proto = filepath:find("^(%a+)://")
+	if proto then
+		log("warn",string.format("Cannot move a streaming file", proto))
+		return nil
+	end
+	return filepath
+end
+
+function platform()
+    return package.config:sub(1,1) == "\\" and "win" or "unix"
+end
+
+function move(filepath)
+	local cmd
+	if platform() == "win" then
+		cmd = string.format('move "%s" "%s"', filepath, dst)
+	else
+		cmd = string.format('mv "%s" "%s"', filepath, dst)
+
+	end
+	log("info",string.format("moving %s to %s", filepath,dst))
+	os.execute(cmd)
+
 end
 
 function move_to()
-  local filepath = check_file()
-  if filepath then
-    local cmd = string.format('move "%s" "%s"', filepath, dst)
-    log("info",string.format("moving %s to %s", filepath,dst))
-    os.execute(cmd)
-  end 
+	local filepath = check_file()
+	if filepath then
+		check_dir()
+		move(filepath)
+	end
 end
 
 mp.add_key_binding('ctrl+m', "move-file", move_to)
